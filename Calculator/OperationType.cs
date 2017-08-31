0b9d297b1e3f40cbf413aa459d2b9f913753e714
Erick Adrian Perez <erick.perez@ldschurch.org>
2017-08-31 10:57:42 -0600

    First commit

---
+++ Calculator/OperationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCalculator
+{
+	class OperationType
+	{
+		public string Operator { get; set; }
+		public static OperationType GetOperator()
+		{
+			var newOperator = new OperationType() { Operator = "Testing 123" };
+			return newOperator;
+		}
+	}
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MyCalculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Erick's Calculator.\n\n");
+
+			while (true)
+			{
+				Console.WriteLine("Enter your first number");
+				float firstNumber = float.Parse(Console.ReadLine());
+
+				Console.WriteLine("Enter operator ( +, -, *, or /)");
+				string operationType = Console.ReadLine();
+
+				while (true)
+				{
+					var regex = @"[*+/-]";
+					var match = Regex.Match(operationType, regex);
+
+					if (!match.Success)
+					{
+						Console.WriteLine("Enter correct operator ( +, -, *, or /)");
+						operationType = Console.ReadLine();
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				//Test
+				OperationType newOperation = OperationType.GetOperator();
+				Console.WriteLine("You selected: {0}", newOperation.Operator);
+
+				Console.WriteLine("Enter your second number");
+				float secondNumber = float.Parse(Console.ReadLine());
+
+				float result;
+
+				switch (operationType)
+				{
+					case "+":
+						result = firstNumber + secondNumber;
+						Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+						break;
+					case "-":
+						result = firstNumber - secondNumber;
+						Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, result);
+						break;
+					case "*":
+						result = firstNumber * secondNumber;
+						Console.WriteLine("{0} x {1} = {2}", firstNumber, secondNumber, result);
+						break;
+					case "/":
+						result = firstNumber / secondNumber;
+						Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
+						break;
+					default:
+						break;
+				}
+
+				Console.WriteLine("One more try?");
+				string response = Console.ReadLine();
+
+				if (response.ToLower() == "no" || response.ToLower() == "quit")
+				{
+					break;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+}
